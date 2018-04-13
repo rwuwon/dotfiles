735b0aca86841ab46390ffc0f731df1a28fe450f
Raymond Wu Won <rwuwon@gmail.com>
2018-04-13 22:58:20 +1000

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+alias l="ls -lah --time-style=long-iso --group-directories-first $argv"
+alias mv="mv -i"
+#alias -='cd -'     # deprecated
+abbr -a -- - 'cd -' # Use this one from version 2.5.0 onwards
+set PATH ~/scripts /usr/local/bin /usr/sbin $PATH
+
+# x11-vim for +clipboard
+alias vi='vimx'
+alias vim='vimx'
