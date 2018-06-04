37592585766d49c4d6de8d3873b6f30fb09ff8bf
Raymond Wu Won <rwuwon@gmail.com>
2018-06-04 23:49:00 +1000

    ls directories & dotfiles added. Reorganised lines

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,18 @@
-alias l="ls -lah --time-style=long-iso --group-directories-first $argv"
-alias ll="ls -lh --time-style=long-iso --group-directories-first $argv"
-alias lll="ls -lhrt --time-style=long-iso $argv"
-alias mv="mv -i"
+set PATH ~/scripts /usr/local/bin /usr/sbin $PATH
+
 #alias -='cd -'     # deprecated
 abbr -a -- - 'cd -' # Use this one from version 2.5.0 onwards
-set PATH ~/scripts /usr/local/bin /usr/sbin $PATH
+
+# bash version: export GPG_TTY=$(tty)
+export GPG_TTY=(tty)
 
 # x11-vim for +clipboard
 alias vi='vimx'
 alias vim='vimx'
-# bash version: export GPG_TTY=$(tty)
-export GPG_TTY=(tty)
+
+alias l="ls -lah --time-style=long-iso --group-directories-first $argv"
+alias ll="ls -lh --time-style=long-iso --group-directories-first $argv"
+alias lll="ls -lahrt --time-style=long-iso $argv"
+alias l.='ls -d .* --time-style=long-iso'
+
+alias mv="mv -i"
