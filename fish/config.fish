7a930011847ae14821ed1241c566762e472a61ae
Raymond Wu Won <rwuwon@gmail.com>
2018-06-05 10:03:26 +1000

    Quote marks consistencised; dotfiles versioned

--- fish/config.fish
+++ fish/config.fish
@@ -11,9 +11,13 @@ export GPG_TTY=(tty)
 alias vi='vimx'
 alias vim='vimx'
 
-alias l="ls -lah --time-style=long-iso --group-directories-first $argv"
-alias ll="ls -lh --time-style=long-iso --group-directories-first $argv"
-alias lll="ls -lahrt --time-style=long-iso $argv"
+alias l='ls -lah --time-style=long-iso --group-directories-first $argv'
+alias ll='ls -lh --time-style=long-iso --group-directories-first $argv'
+alias lll='ls -lahrt --time-style=long-iso $argv'
 alias l.='ls -d .* --time-style=long-iso'
 
-alias mv="mv -i"
+alias mv='mv -i'
+
+# https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
+# https://news.ycombinator.com/item?id=11071754
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
