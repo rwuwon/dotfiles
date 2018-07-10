1645afe70c85b03a5ab2ef8eebbbe6859ad72a42
Raymond Wu Won <rwuwon@gmail.com>
2018-07-10 12:57:52 +1000

    Add Display Power Management Signaling settings

--- scripts/keyboardmouse.sh
+++ scripts/keyboardmouse.sh
@@ -7,3 +7,8 @@ xset r rate 250 40
 # Handled instead by /etc/X11/xorg.conf.d/30-touchpad.conf
 # https://wiki.archlinux.org/index.php/Libinput#Change_touchpad_sensitivity
 #xinput set-prop 11 287 1
+
+# https://wiki.archlinux.org/index.php/Display_Power_Management_Signaling
+# Use xset q to query
+xset dpms 300 300 300
+xset s 300 300
