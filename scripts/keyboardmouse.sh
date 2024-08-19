46497b94148d5ace0e78fa64cc5fce06f75848d7
Raymond Wu Won <rwuwon@gmail.com>
2024-08-19 21:52:35 +1000

    Clean up mostly-redundant keyboardmouse.sh

--- scripts/keyboardmouse.sh
+++ scripts/keyboardmouse.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+# This script has mostly been replaced with ~/.profile and /etc/X11/xorg.conf.d
+# xset r rate remains for desktop keyboard hot-plugging
+
+# Previous default of 250 30 was fine; 40 is faster
 xset r rate 250 40
+
+# Mouse accel under Fedora/Gnome is handled via Gnome conf
+#xset m 0 0
 
 # https://wiki.archlinux.org/index.php/Libinput#Via_xinput
 # Reverse scroll direction
@@ -8,7 +15,17 @@ xset r rate 250 40
 # https://wiki.archlinux.org/index.php/Libinput#Change_touchpad_sensitivity
 #xinput set-prop 11 287 1
 
+# This will change if the USB legacy support setting is changed in BIOS
+# type "xinput list" to set the right mouse prop and then:
+# xinput list-props 'Kingsis Peripherals ZOWIE Gaming mouse'|grep Coordinate
+# 9 is id for mouse, 154 is Coordinate Transformation Matrix for mouse
+
+# 2 is double speed for 400 dpi mouse setting. Works well at 1080p.
+#xinput set-prop 9 154 2 0 0 0 2 0 0 0 1 # speed for red light
+#xinput set-prop 9 154 0 0 0 0 0 0 0 0 1 # speed for pink light
+# source: https://wiki.ubuntu.com/X/InputCoordinateTransformation
+
 # https://wiki.archlinux.org/index.php/Display_Power_Management_Signaling
 # Use xset q to query
-xset dpms 300 300 300
-xset s 300 300
+#xset dpms 300 300 300
+#xset s 300 300
