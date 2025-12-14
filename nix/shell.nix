b83c754ea7bce47b161f01fbb744ace813b172df
Raymond Wu Won <rwuwon@gmail.com>
2025-12-15 09:28:14 +1000

    Add emoji-picker

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,27 +1,28 @@
 let
-	nixpkgs = fetchTarball "https://github.com/NixOs/nixpkgs/tarball/nixos-25.05";
-	pkgs = import nixpkgs { config = {}; overlays = []; };
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 
 pkgs.mkShellNoCC {
-	packages = with pkgs; [
-		cowsay
-		lolcat
-		curl
-		inetutils
-		sl
-		vim
-		tmux
-		btop
-		htop
-		gdu
-		openssh
-	];
+  packages = with pkgs; [
+    cowsay
+    lolcat
+    cyberchef
+    emoji-picker
+    curl
+    inetutils
+    sl
+    vim
+    tmux
+    btop
+    htop
+    gdu
+    openssh
+  ];
 
-	GREETING = "Hello, Nix!";
+  GREETING = "Hello, Nix!";
 
-	shellHook = ''
-		echo $GREETING | cowsay | lolcat
-		# curl --version
-	'';
+  shellHook = ''
+    echo $GREETING | cowsay | lolcat
+  '';
 }
