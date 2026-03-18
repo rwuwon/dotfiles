103fcde1fbbd41a3a6359223507051aca459498d
Raymond Wu Won <rwuwon@gmail.com>
2026-03-18 17:02:34 +1000

    Add nix-shell channel tips

--- nix/shell.nix
+++ nix/shell.nix
@@ -8,12 +8,18 @@ pkgs.mkShellNoCC {
   packages = with pkgs; [
     cowsay
     lolcat
+    curl
   ];
 
   GREETING = "Hello, Nix!";
 
   shellHook = ''
     echo $GREETING | cowsay | lolcat
-    echo Packages in this nix-shell: cowsay lolcat
+    echo -e "\nPackages in this nix-shell: cowsay lolcat curl\n"
+    curl -V
+    echo -e "\nTips:"
+    echo -e "-----"
+    echo nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/tarball/nixos-25.11 -p curl --run curl -V
+    echo nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz -p curl --run curl -V
   '';
 }
