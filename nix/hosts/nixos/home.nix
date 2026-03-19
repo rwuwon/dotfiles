b4b9fc5e4d33f26c29b12003189cf3439d83d6bf
Raymond Wu Won <rwuwon@gmail.com>
2026-03-19 23:35:54 +1000

    Change fish prompt

--- nix/hosts/nixos/home.nix
+++ nix/hosts/nixos/home.nix
@@ -223,38 +223,7 @@
     # tmux: Safer to not source, to maintain set-option -g default-shell
 
     ".config/fish/config.fish".source = fish/config.fish;
-    ".config/fish/conf.d/fish_prompt.fish" = {
-      text = ''
-      function fish_prompt --description 'Informative prompt'
-        #Save the return status of the previous command
-        set -l last_pipestatus $pipestatus
-        set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
-
-        if functions -q fish_is_root_user; and fish_is_root_user
-          printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
-            and set_color $fish_color_cwd_root
-            or set_color $fish_color_cwd) \
-            (prompt_pwd) (set_color normal)
-        else
-          set -l status_color (set_color $fish_color_status)
-          set -l statusb_color (set_color --bold $fish_color_status)
-          set -l pipestatus_string (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
-
-          printf '[%s] %s%s@%s %s%s %s%s%s \n> ' (date "+%H:%M:%S") (set_color brblue) \
-            $USER (prompt_hostname) (set_color $fish_color_cwd) $PWD $pipestatus_string \
-            (set_color normal)
-
-        set -l nix_shell_info (
-          if test -n "$IN_NIX_SHELL"
-            echo -n "[nix-shell]\$ "
-          end
-        )
-        echo -n -s "$nix_shell_info"
-
-        end
-      end
-      '';
-    };
+    ".config/fish/functions/fish_prompt.fish".source = fish/fish_prompt.fish;
 
     # Sourcing vim/myfiletypes.vim doesn't seem to work for some reason.
     ".vim/myfiletypes.vim" = {
