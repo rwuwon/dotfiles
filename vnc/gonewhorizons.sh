4c41b00011e98d8d8951df33de422d67711482e4
Raymond Wu Won <rwuwon@gmail.com>
2026-06-24 00:14:14 +1000

    newhorizons now running Debian 13 for now
    macOS Monterey (version 12) accidentally hosed in the process of an
    OpenCore Legacy Patcher upgrade attempt.

--- vnc/gonewhorizons.sh
+++ vnc/gonewhorizons.sh
@@ -7,4 +7,13 @@
 # ssh -NTL 5903:localhost:5900 io@newhorizons
 
 #vncviewer localhost:5903 -AlwaysCursor -CursorType=System -CompressLevel=9 -QualityLevel=0 -FullScreen -FullScreenMode=All -LowColorLevel=0  # newhorizons macOS
-vncviewer localhost:5903 -AlwaysCursor -CursorType=System -FullScreen -FullScreenMode=All # newhorizons macOS
+#vncviewer localhost:5903 -AlwaysCursor -CursorType=System -FullScreen -FullScreenMode=All # newhorizons macOS
+
+
+# Post macOS Monterey -> Debian change:
+
+ssh -f -L 5903:localhost:5903 io@newhorizons 'export WLR_BACKENDS=headless && export WLR_LIBINPUT_NO_DEVICES=1 && export WAYLAND_DISPLAY=wayland-1 && wayvnc localhost 5907' && sleep 2; vncviewer localhost:5903
+echo "\n\n========================"
+echo "Final step if necessary:"
+echo "vncviewer localhost:5903"
+echo "========================\n\n"
