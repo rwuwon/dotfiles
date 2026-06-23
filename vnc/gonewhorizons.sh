332aa63d12c6074baf75128d48a559fc041dd8ea
Raymond Wu Won <rwuwon@gmail.com>
2026-06-24 00:49:45 +1000

    Fix wayvnc port

--- vnc/gonewhorizons.sh
+++ vnc/gonewhorizons.sh
@@ -2,7 +2,7 @@
 
 # Post macOS Monterey -> Debian change:
 
-ssh -f -L 5903:localhost:5903 io@newhorizons 'export WLR_BACKENDS=headless && export WLR_LIBINPUT_NO_DEVICES=1 && export WAYLAND_DISPLAY=wayland-1 && wayvnc localhost 5907' && sleep 2; vncviewer localhost:5903
+ssh -f -L 5903:localhost:5903 io@newhorizons 'export WLR_BACKENDS=headless && export WLR_LIBINPUT_NO_DEVICES=1 && export WAYLAND_DISPLAY=wayland-1 && wayvnc localhost 5903' && sleep 2; vncviewer localhost:5903
 echo "\n\n========================"
 echo "Final step if necessary:"
 echo "vncviewer localhost:5903"
