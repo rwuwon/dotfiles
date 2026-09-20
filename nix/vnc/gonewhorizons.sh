c14fe316dd55a0b2c0818206690fa1fe55414973
Raymond Wu Won <rwuwon@gmail.com>
2026-09-20 15:10:34 +1000

    Reformat long lines

--- nix/vnc/gonewhorizons.sh
+++ nix/vnc/gonewhorizons.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
 # Post macOS Monterey -> Debian change:
-echo "ssh -f -L 5903:localhost:5903 io@newhorizons 'pkill wayvnc && export WLR_BACKENDS=headless && export WLR_LIBINPUT_NO_DEVICES=1 && export WAYLAND_DISPLAY=wayland-1 && wayvnc 172.16.3.3 5903' && sleep 2 && vncviewer 172.16.3.3:5903 -MenuKey=Home -FullScreen=0"
+echo "ssh -f -L 5903:localhost:5903 io@newhorizons 'pkill wayvnc && \
+export WLR_BACKENDS=headless && \
+export WLR_LIBINPUT_NO_DEVICES=1 && \
+export WAYLAND_DISPLAY=wayland-1 && \
+wayvnc 172.16.3.3 5903' && \
+sleep 2 && \
+vncviewer 172.16.3.3:5903 -MenuKey=Home -FullScreen=0"
+
 echo "======================================\n\n"
 
-ssh -f -L 5903:localhost:5903 io@newhorizons 'pkill wayvnc && export WLR_BACKENDS=headless && export WLR_LIBINPUT_NO_DEVICES=1 && export WAYLAND_DISPLAY=wayland-1 && wayvnc 172.16.3.3 5903' && sleep 2 && vncviewer 172.16.3.3:5903 -MenuKey=Home -FullScreen=0
+ssh -f -L 5903:localhost:5903 io@newhorizons 'pkill wayvnc && \
+export WLR_BACKENDS=headless && \
+export WLR_LIBINPUT_NO_DEVICES=1 && \
+export WAYLAND_DISPLAY=wayland-1 && \
+wayvnc 172.16.3.3 5903' && \
+sleep 2 && \
+vncviewer 172.16.3.3:5903 -MenuKey=Home -FullScreen=0
+
 echo "\n\n======================================"
+
 echo "Reconnect using one of:"
 echo "vncviewer 172.16.3.3:5903 -MenuKey=Home -FullScreen=0 -AutoSelect=0 -CompressLevel=9 -QualityLevel=2 -LowColorLevel=0 #newhorizons"
 echo "vncviewer 172.16.3.3:5903 -MenuKey=Home -FullScreen=0 #newhorizons"
--- nix/vnc/gonix.sh
+++ nix/vnc/gonix.sh
@@ -5,21 +5,35 @@
 # https://github.com/LibVNC/x11vnc/?tab=readme-ov-file#readme
 #
 # https://unix.stackexchange.com/questions/659362/how-to-generate-correctly-a-new-xauthority-file-for-a-display-missing-it-witho
-#
+
 # Use ssh -f for background, and ssh -t for pseudo-terminal
-
-
-# One-step:
-
-ssh -f -L 5905:localhost:5905 io@nix "export WLR_BACKENDS=headless && export WLR_LIBINPUT_NO_DEVICES=1 && export WAYLAND_DISPLAY=wayland-0 && wayvnc localhost 5905" && sleep 2; vncviewer localhost:5905 -MenuKey=Home
-echo "\n\n========================"
-echo "Final step if necessary:"
-echo "vncviewer localhost:5905 -MenuKey=Home"
-echo "========================\n\n"
-
 
 # For X11 VMs:
 # sway to i3 with Xauthority auth set
 # -auth tip: ps wwwwaux | grep auth
 
 #ssh -f -L 5905:localhost:5905 io@nix 'x11vnc -localhost -autoport 5905 -display :0 -forever -noxdamage -repeat -nowf -auth /run/user/1000/gdm/Xauthority' && sleep 2; vncviewer localhost:5905
+
+# Wayland, in one-step:
+
+echo "ssh -f -L 5905:localhost:5905 io@nix 'export WLR_BACKENDS=headless && \
+export WLR_LIBINPUT_NO_DEVICES=1 && \
+export WAYLAND_DISPLAY=wayland-0 && \
+wayvnc -v localhost 5905' && \
+sleep 5 && \
+vncviewer localhost:5905 -MenuKey=Home -FullScreen=0"
+
+echo "\n========================================================="
+echo "Troubleshooting:"
+echo "vncviewer localhost:5905 -MenuKey=Home -FullScreen=0 #nix"
+echo "---------------------------------------------------------"
+echo "Reconnect with:"
+echo "ssh -t -L 5905:localhost:5905 io@nix 'pkill wayvnc'"
+echo "=========================================================\n"
+
+ssh -f -L 5905:localhost:5905 io@nix 'export WLR_BACKENDS=headless && \
+export WLR_LIBINPUT_NO_DEVICES=1 && \
+export WAYLAND_DISPLAY=wayland-0 && \
+wayvnc -v localhost 5905' && \
+sleep 5 && \
+vncviewer localhost:5905 -MenuKey=Home -FullScreen=0
